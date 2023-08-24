8540be885794631b64f7d6f6a113907ee598921e
kitamura-ryusei <141385505+kitamura-ryusei@users.noreply.github.com>
2023-08-24 08:26:49 +0000

    プロフィール編集完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,8 @@ class UsersController < ApplicationController
     redirect_to user_path
   end
     
+  private
+  
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
